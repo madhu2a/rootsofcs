8788ee9fdc623cac7a06249465ebeaaecc846d49
v-madava <v-madava@BCSR-MAVANCHA>
2018-02-13 11:31:37 -0800

    BinarySearchTree

---
+++ Trees/BinaryTree.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees
+{
+    public class BinarySearchTree
+    {
+        public bool FindElement(int[] tree, int element)
+        {
+            int left = 0; // left most
+            int right = tree.Length; // right most
+
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (tree[mid] == element)
+                {
+                    return true;
+                }
+
+                if (tree[mid] < element)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return false;
+        }
+    }
+}
--- Trees/Program.cs
+++ Trees/Program.cs
@@ -10,6 +10,13 @@ namespace Trees
     {
         static void Main(string[] args)
         {
+            int[] data = new int[] { 1, 5, 6, 8, 13, 45, 65, 121, 123, 163, 245, 334 };
+            int target = 123;
+            BinarySearchTree bst = new BinarySearchTree();
+
+            var result = bst.FindElement(data, target) ? "Found element" : "Not found";
+
+            Console.WriteLine(result);
         }
     }
 }
